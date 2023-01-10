faabf4e1abddd4e3d5475e205e8eca1ab73ecf91
Craig Barnes <51026305+craigyjp@users.noreply.github.com>
2023-01-10 17:30:46 +0200

    Fixed clock signal - no output

--- Elkyam_MIDI_CV_Aftertouch_MCP4922.ino
+++ Elkyam_MIDI_CV_Aftertouch_MCP4922.ino
@@ -17,16 +17,16 @@
 #include <SPI.h>
 #include <MCP4922.h>
 
-#define DAC_MOSI  11
-#define DAC_SCK  13
-#define DAC_LDAC  9
-#define DAC_CS1  10
-#define DAC_CS2  8
-#define DAC_CS3  7
+#define DAC_MOSI 11
+#define DAC_SCK 13
+#define DAC_LDAC 9
+#define DAC_CS1 10
+#define DAC_CS2 8
+#define DAC_CS3 7
 
-MCP4922 DAC1(DAC_MOSI,DAC_SCK,DAC_CS1,DAC_LDAC); 
-MCP4922 DAC2(DAC_MOSI,DAC_SCK,DAC_CS2,DAC_LDAC); 
-MCP4922 DAC3(DAC_MOSI,DAC_SCK,DAC_CS3,DAC_LDAC);  // (MOSI,SCK,CS,LDAC) define Connections for MEGA_board, 
+MCP4922 DAC1(DAC_MOSI, DAC_SCK, DAC_CS1, DAC_LDAC);
+MCP4922 DAC2(DAC_MOSI, DAC_SCK, DAC_CS2, DAC_LDAC);
+MCP4922 DAC3(DAC_MOSI, DAC_SCK, DAC_CS3, DAC_LDAC);  // (MOSI,SCK,CS,LDAC) define Connections for MEGA_board,
 
 // Note priority is set by pins A0 and A2
 // Highest note priority: A0 and A2 high (open)
@@ -36,14 +36,14 @@ MCP4922 DAC3(DAC_MOSI,DAC_SCK,DAC_CS3,DAC_LDAC);  // (MOSI,SCK,CS,LDAC) define C
 #define NP_SEL1 A0  // Note priority is set by pins A0 and A2
 #define NP_SEL2 A2
 
-#define GATE  2
-#define TRIG  3
+#define GATE 2
+#define TRIG 3
 #define CLOCK 4
 
-#define MIDI0  A1
-#define MIDI1  A3
-#define MIDI2  A6
-#define MIDI3  A7
+#define MIDI0 A1
+#define MIDI1 A3
+#define MIDI2 A6
+#define MIDI3 A7
 
 int MIDI_CHANNEL = 0;
 int SWITCH1 = 0;
@@ -53,8 +53,7 @@ int SWITCH4 = 0;
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-void setup()
-{
+void setup() {
   pinMode(NP_SEL1, INPUT_PULLUP);
   pinMode(NP_SEL2, INPUT_PULLUP);
 
@@ -71,58 +70,55 @@ void setup()
   digitalWrite(CLOCK, LOW);
 
   SPI.begin();
+  SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
   MIDI.begin(MIDI_CHANNEL);
 
   // Set initial pitch bend voltage to 0.5V (mid point).  With Gain = 1X, this is 1023
   // Other DAC outputs will come up as 0V, so don't need to be initialized
-  setVoltage2(0, 2047);
+  delay(50);
+  DAC2.Set(819, 0);
 }
 
-bool notes[88] = {0};
-int8_t noteOrder[20] = {0}, orderIndx = {0};
+bool notes[88] = { 0 };
+int8_t noteOrder[20] = { 0 }, orderIndx = { 0 };
 unsigned long trigTimer = 0;
 unsigned int mV;
 int velocity, newd2, newerd2;
 
-void loop()
-{
+void loop() {
   int type, noteMsg, channel, d1, d2;
   static unsigned long clock_timer = 0, clock_timeout = 0;
   static unsigned int clock_count = 0;
   bool S1, S2;
 
   if ((trigTimer > 0) && (millis() - trigTimer > 20)) {
-    digitalWrite(TRIG, LOW); // Set trigger low after 20 msec
+    digitalWrite(TRIG, LOW);  // Set trigger low after 20 msec
     trigTimer = 0;
   }
 
   if ((clock_timer > 0) && (millis() - clock_timer > 20)) {
-    digitalWrite(CLOCK, LOW); // Set clock pulse low after 20 msec
+    digitalWrite(CLOCK, LOW);  // Set clock pulse low after 20 msec
     clock_timer = 0;
   }
 
   if (digitalRead(MIDI0) == 1) {
     SWITCH1 = 1;
-  }
-  else {
+  } else {
     SWITCH1 = 0;
   }
   if (digitalRead(MIDI1) == 1) {
     SWITCH2 = 2;
-  }
-  else {
+  } else {
     SWITCH2 = 0;
   }
   if (digitalRead(MIDI2) == 1) {
     SWITCH3 = 4;
-  }
-  else {
+  } else {
     SWITCH3 = 0;
   }
   if (digitalRead(MIDI3) == 1) {
     SWITCH4 = 8;
-  }
-  else {
+  } else {
     SWITCH4 = 0;
   }
   MIDI_CHANNEL = (SWITCH1 + SWITCH2 + SWITCH3 + SWITCH4);
@@ -131,39 +127,38 @@ void loop()
   if (MIDI.read(MIDI_CHANNEL)) {
     byte type = MIDI.getType();
     switch (type) {
+
       case midi::NoteOn:
       case midi::NoteOff:
-        noteMsg = MIDI.getData1() - 24; // A0 = 21, Top Note = 108
+        noteMsg = MIDI.getData1() - 24;  // A0 = 21, Top Note = 108
 
         channel = MIDI.getChannel();
 
-        if ((noteMsg < 0) || (noteMsg > 87)) break; // Only 88 notes of keyboard are supported
+        if ((noteMsg < 0) || (noteMsg > 87)) break;  // Only 88 notes of keyboard are supported
 
         if (type == midi::NoteOn) velocity = MIDI.getData2();
-        else velocity  = 0;
+        else velocity = 0;
 
-        if (velocity == 0)  {
+        if (velocity == 0) {
           notes[noteMsg] = false;
-        }
-        else {
+        } else {
           notes[noteMsg] = true;
           // velocity range from 0 to 4095 mV  Left shift d2 by 5 to scale from 0 to 4095,
           // and choose gain = 2X
           int newervol = velocity << 5;
-          setVoltage1(mV, newervol); // DAC1, channel 1, gain = 2X
+          //setVoltage1(mV, newervol); // DAC1, channel 1, gain = 2X
+          DAC1.Set(mV, newervol);
         }
 
         // Pins NP_SEL1 and NP_SEL2 indictate note priority
         S1 = digitalRead(NP_SEL1);
         S2 = digitalRead(NP_SEL2);
 
-        if (S1 && S2) { // Highest note priority
+        if (S1 && S2) {  // Highest note priority
           commandTopNote();
-        }
-        else if (!S1 && S2) { // Lowest note priority
+        } else if (!S1 && S2) {  // Lowest note priority
           commandBottomNote();
-        }
-        else { // Last note priority
+        } else {                 // Last note priority
           if (notes[noteMsg]) {  // If note is on and using last note priority, add to ordered list
             orderIndx = (orderIndx + 1) % 20;
             noteOrder[orderIndx] = noteMsg;
@@ -174,46 +169,45 @@ void loop()
 
       case midi::PitchBend:
         d1 = MIDI.getData1();
-        d2 = MIDI.getData2(); // d2 from 0 to 127, mid point = 64
+        d2 = MIDI.getData2();  // d2 from 0 to 127, mid point = 64
 
         // Pitch bend output from 0 to 1023 mV.  Left shift d2 by 4 to scale from 0 to 2047.
         // With DAC gain = 1X, this will yield a range from 0 to 1023 mV.
         newd2 = d2 << 4;
-        setVoltage2(newd2, newerd2); // DAC2, channel 0, gain = 1X
+        //setVoltage2(newd2, newerd2); // DAC2, channel 0, gain = 1X
+        DAC2.Set(newd2, newerd2);
 
         break;
 
       case midi::ControlChange:
         d1 = MIDI.getData1();
-        d2 = MIDI.getData2(); // From 0 to 127
+        d2 = MIDI.getData2();  // From 0 to 127
 
         // CC range from 0 to 4095 mV  Left shift d2 by 5 to scale from 0 to 4095,
         // and choose gain = 2X
         newerd2 = d2 << 5;
-        setVoltage2(newd2, newerd2); // DAC2, channel 1, gain = 2X
+        //setVoltage2(newd2, newerd2); // DAC2, channel 1, gain = 2X
+        DAC2.Set(newd2, newerd2);
         break;
 
-      case midi::AfterTouchChannel:
-        d1 = MIDI.getData1();
-
-        // CC range from 0 to 4095 mV  Left shift d2 by 5 to scale from 0 to 4095, 
-        // and choose gain = 2X
-        int newd1 = d1 << 5;
-        setVoltage3(newd1, 2047); // DAC2, channel 1, gain = 2X
-        break;
-        
       case midi::Clock:
-        if (millis() > clock_timeout + 300) clock_count = 0; // Prevents Clock from starting in between quarter notes after clock is restarted!
+        if (millis() > clock_timeout + 300) clock_count = 0;  // Prevents Clock from starting in between quarter notes after clock is restarted!
         clock_timeout = millis();
 
         if (clock_count == 0) {
-          digitalWrite(CLOCK, HIGH); // Start clock pulse
+          digitalWrite(CLOCK, HIGH);  // Start clock pulse
           clock_timer = millis();
         }
         clock_count++;
         if (clock_count == 24) {  // MIDI timing clock sends 24 pulses per quarter note.  Sent pulse only once every 24 pulses
           clock_count = 0;
         }
+        break;
+
+      case midi::AfterTouchChannel:
+        d1 = MIDI.getData1();
+        int newd1 = d1 << 5;
+        DAC3.Set(newd1, 0);  // DAC2, channel 1, gain = 2X
         break;
 
       case midi::ActiveSensing:
@@ -226,13 +220,11 @@ void loop()
   }
 }
 
-void commandTopNote()
-{
+void commandTopNote() {
   int topNote = 0;
   bool noteActive = false;
 
-  for (int i = 0; i < 88; i++)
-  {
+  for (int i = 0; i < 88; i++) {
     if (notes[i]) {
       topNote = i;
       noteActive = true;
@@ -241,17 +233,15 @@ void commandTopNote()
 
   if (noteActive)
     commandNote(topNote);
-  else // All notes are off, turn off gate
+  else  // All notes are off, turn off gate
     digitalWrite(GATE, LOW);
 }
 
-void commandBottomNote()
-{
+void commandBottomNote() {
   int bottomNote = 0;
   bool noteActive = false;
 
-  for (int i = 87; i >= 0; i--)
-  {
+  for (int i = 87; i >= 0; i--) {
     if (notes[i]) {
       bottomNote = i;
       noteActive = true;
@@ -260,67 +250,37 @@ void commandBottomNote()
 
   if (noteActive)
     commandNote(bottomNote);
-  else // All notes are off, turn off gate
+  else  // All notes are off, turn off gate
     digitalWrite(GATE, LOW);
 }
 
-void commandLastNote()
-{
+void commandLastNote() {
 
   int8_t noteIndx;
 
   for (int i = 0; i < 20; i++) {
-    noteIndx = noteOrder[ mod(orderIndx - i, 20) ];
+    noteIndx = noteOrder[mod(orderIndx - i, 20)];
     if (notes[noteIndx]) {
       commandNote(noteIndx);
       return;
     }
   }
-  digitalWrite(GATE, LOW); // All notes are off
+  digitalWrite(GATE, LOW);  // All notes are off
 }
-
-// Rescale 88 notes to 4096 mV:
-//    noteMsg = 0 -> 0 mV
-//    noteMsg = 87 -> 4096 mV
-// DAC output will be (4095/87) = 47.069 mV per note, and 564.9655 mV per octive
-// Note that DAC output will need to be amplified by 1.77X for the standard 1V/octave
-
-#define NOTE_SF 36.995f // This value can be tuned if CV output isn't exactly 1V/octave
+//#define NOTE_SF 48.017f
+#define NOTE_SF 36.993f  // This value can be tuned if CV output isn't exactly 1V/octave
 
 void commandNote(int noteMsg) {
   digitalWrite(GATE, HIGH);
   digitalWrite(TRIG, HIGH);
   trigTimer = millis();
 
-  mV = (unsigned int) ((float) (noteMsg) * NOTE_SF + 0.5);
+  mV = (unsigned int)((float)(noteMsg)*NOTE_SF + 0.5);
   int newvel = velocity << 5;
-  setVoltage1(mV, newvel);  // DAC1, channel 0, gain = 2X
+  DAC1.Set(mV, newvel);  // DAC1, channel 0, gain = 2X
 }
 
-// setVoltage -- Set DAC voltage output
-// dacpin: chip select pin for DAC.  Note and velocity on DAC1, pitch bend and CC on DAC2
-// channel: 0 (A) or 1 (B).  Note and pitch bend on 0, velocity and CC on 2.
-// gain: 0 = 1X, 1 = 2X.
-// mV: integer 0 to 4095.  If gain is 1X, mV is in units of half mV (i.e., 0 to 2048 mV).
-// If gain is 2X, mV is in units of mV
-
-void setVoltage1(unsigned int DAC1channel1, int DAC1channel2)
-{
-  DAC1.Set(DAC1channel1,DAC1channel2); 
-}
-
-void setVoltage2(int DAC2channel1, int DAC2channel2)
-{
-  DAC2.Set(DAC2channel1,DAC2channel2); 
-}
-
-void setVoltage3(int DAC3channel1, int DAC3channel2)
-{
-  DAC3.Set(DAC3channel1,DAC3channel2); 
-}
-
-int mod(int a, int b)
-{
+int mod(int a, int b) {
   int r = a % b;
   return r < 0 ? r + b : r;
 }
